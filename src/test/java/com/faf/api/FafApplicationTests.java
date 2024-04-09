634c65ee8183a50e2667f62e1da299dca75cc7c2
Daniel Shelest <danilisan321@gmail.com>
2024-04-09 19:21:03 +0000

    Resolve FF-22

--- src/test/java/com/faf/api/FafApplicationTests.java
+++ src/test/java/com/faf/api/FafApplicationTests.java
@@ -1,9 +1,11 @@
 package com.faf.api;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 public class FafApplicationTests {
     @Test
     void contextLoads() {
